637b05ad7861187d15f1bea36ae6d7c6c29c3854
[Shubrajit] <shubrajitdeb180603@gmail.com>
2024-01-23 23:08:15 +0530

    basic setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(`url shortener`)
+const express=require("express")
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.end("hello from server")
+})
+const PORT=3000;
+app.listen(process.env.PORT||3000,()=>{
+    console.log('server started at localhost http://localhost:3000')
+})
+
